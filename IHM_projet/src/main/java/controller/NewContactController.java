9bf6a05082b06bf93a43d15a223749208f81e069
heihong <traingheihong@gmail.com>
2014-09-26 22:17:17 +0200

    file newContact

---
+++ IHM_projet/src/main/java/controller/NewContactController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import model.Contact;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/NewContact")
+public class NewContactController {
+	
+	@RequestMapping(value = "/newContact",method = RequestMethod.GET)
+	public String newContact(Model model){
+		model.addAttribute("contact", new Contact()); //model associé à la vue
+		
+		return "newcontact";// jsp file
+	}
+
+	
+
+}
---
+++ IHM_projet/src/main/java/model/Contact.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
