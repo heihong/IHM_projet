90c5b254875a149e3344de83d34d10448e795933
Vincent <Vincent@Vincent-PC>
2014-09-27 19:16:11 +0200

    Add model Address

---
+++ IHM_projet/src/main/java/model/Address.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Address {
+
+	private String number;
+	private String street;
+	private String postCode;
+	private String city;
+
+	public Address() {
+	}
+	
+	public Address(String number, String street, String postCode, String city) {
+		this.number = number;
+		this.street = street;
+		this.postCode = postCode;
+		this.city = city;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
